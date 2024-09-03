7e6e301a85e6aafdbfa268f33287b60714cd45d5
PNS212624\Hansol <yrlee00@hansol.com>
2024-09-03 17:39:19 +0900

    Feat: 프로젝트 생성

---
+++ front/App.tsx
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+
+function App(): React.JSX.Element {
+  
+  return ( 
+    <View>
+      <Text>테스트</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'red'
+  }
+});
+
+export default App;
